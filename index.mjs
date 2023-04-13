2c403c8b30cb7ac364c0e66fb51bc0bd46570c6d
Joacim Magnusson <joacim@isogram.se>
2023-04-13 16:06:13 +0200

    Increase max tokens

--- index.mjs
+++ index.mjs
@@ -56,10 +56,10 @@ async function generateSummary(prompt) {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
-      max_tokens: 512, // Adjust the number of tokens based on the desired length of the summary
+      max_tokens: 1024, // Adjust the number of tokens based on the desired length of the summary
       n: 1,
       stop: null,
-      temperature: 0.7,
+      temperature: 0.2,
     });
 
     const summary = response.data.choices[0].text;
@@ -101,7 +101,7 @@ function createPrompt(pullRequestDetails) {
 
   const summarizedDiff = summarizeDiff(diff);
   const commitMessages = commits.map((commit) => commit.message).join("\n");
-  const prompt = `Please summarize the changes in the following pull request. Base the summary on the provided diff and title.\n\nPull Request Title: ${title}\nDiff:\n${summarizedDiff}\n`;
+  const prompt = `Please summarize the changes in the following pull request. Base the summary on the provided diff and title, and try to be clear and specific.\n\nTitle: ${title}\nDiff:\n${summarizedDiff}\n`;
 
   return prompt;
 }
